165d9b14ce7fea2ab147f8a93edf4c38ca79bbd0
Rafał Solnica <granadag99@gmail.com>
2023-11-13 20:38:09 +0100

    added globalstyle and theme

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+  color: {
+    codGrey: "#222",
+    mineShaft: "#333",
+    tundora: "#444",
+    emperor: "#555",
+    doveGrey: "#666",
+    silverChalice: "#aaa",
+    silver: "#ccc",
+    white: "#fff",
+    transparentGrey: "#21212180",
+    semiTransparentGrey: "#212121b3",
+    errorRed: "#b3160b",
+  },
+  breakpoint: { mobileMax: 767 },
+};
