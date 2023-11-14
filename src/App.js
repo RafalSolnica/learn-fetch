e72f5fbd435c6681e3f14621406ba1bc3b29a2a4
Rafał Solnica <granadag99@gmail.com>
2023-11-14 13:01:32 +0100

    created utils.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Container } from "./Container";
+import { hello } from "./utils";
 
 const App = () => {
+  hello();
+
   return (
     <Container>
       <h1>Hello World!</h1>
-      <p>Don't panic tt's just a simple test.</p>
+      <p>Don't panic it's just a simple test.</p>
     </Container>
   );
 };
